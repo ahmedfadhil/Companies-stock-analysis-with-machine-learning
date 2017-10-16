c7f164298d5accf35e086655eb0744616a673374
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-10-16 23:32:30 +0200

    machine learning model added and train test created

--- compml.py
+++ compml.py
@@ -1,7 +1,11 @@
+from collections import Counter
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
+from sklearn.model_selection import train_test_split
+from sklearn import svm, cross_validation, neighbors
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
 
 
 def process_data_for_label(ticker):
@@ -28,3 +32,48 @@ def buy_sell_hold(*args):
             return -1
 
     return 0
+
+
+def extract_featuresets(ticker):
+    tickers, df = process_data_for_label(ticker)
+    df['{}_target'.format(ticker)] = list(map(buy_sell_hold,
+                                              df['{}_1d'.format(ticker)],
+                                              df['{}_2d'.format(ticker)],
+                                              df['{}_3d'.format(ticker)],
+                                              df['{}_4d'.format(ticker)],
+                                              df['{}_5d'.format(ticker)],
+                                              df['{}_6d'.format(ticker)],
+                                              df['{}_7d'.format(ticker)]))
+    vals = df['{}_target'.format(ticker)].values.tolist()
+    str_vals = [str(i) for i in vals]
+    print('Data spread:', Counter(str_vals))
+    df.fillna(0, inplace=True)
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.dropna(inplace=True)
+    df_vals = df[[ticker for ticker in tickers]].pct_change()
+    df_vals = df_vals.replace([np.inf, -np.inf], 0)
+    df_vals.fillna(0, inplace=0)
+    X = df_vals.values
+    y = df['{}_target'.format(ticker)].values
+
+    return X, y, df
+
+
+def do_ml(ticker):
+    X, y, df = extract_featuresets(ticker)
+
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.25, random_state=42)
+
+    # clf = neighbors.KNeighborsClassifier()
+    clf = VotingClassifier(
+        [('lsvc', svm.LinearSVC()), ('knn', neighbors.KNeighborsClassifier()), ('rfor', RandomForestClassifier())])
+    clf.fit(X_train, y_train)
+    confidence = clf.score(X_test, y_test)
+    print('Accuracy:', confidence)
+    predictions = clf.predict(X_test)
+    print('predicted spread:', Counter(predictions))
+    return confidence
+
+
+do_ml('BAC')
+extract_featuresets('XOM')
